6249f16bc9fe709d4ed9975f7ee6a13b825b7fd8
Deepakkumar757 <deepakumar0220@gmail.com>
2023-08-15 17:02:00 +0530

    profile updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 const App = () => {
   return (
-    <div>Hello, I am Deepakkumar</div>
+    <div>
+    <h3>I'm a full stack web developer mainly focusing on backend + devops.</h3>
+    <h4>Technologies that I'm familiar with:</h4>
+    <h4>- React/Nextjs, Node, Sql databases </h4>
+    <h4>Resume:<a href="/resume.html">Click here</a></h4>
+    <h4>Connect with me via:</h4>
+    <h4>Github - <a href="https://github.com/Deepakkumar757">Link</a></h4>
+    <h4>Email - athithyanr4@gmail.com</h4></div>
   )
 }
 
