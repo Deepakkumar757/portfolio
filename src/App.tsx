0e81b04f3cdb72754081e3b4f72ef5fe33609885
Deepakkumar-kaaylabs <deepakkumar.dhandapani@kaaylabs.com>
2023-10-24 11:11:46 +0530

    resume updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
     <h3>I'm a full stack web developer mainly focusing on backend + devops.</h3>
     <h4>Technologies that I'm familiar with:</h4>
     <h4>- React/Nextjs, Node, Sql databases </h4>
-    <h4>Resume:<a href="/resume.html">Click here</a></h4>
+    <h4>Resume:<a href="/resume.pdf" target="blank">Click here</a></h4>
     <h4>Connect with me via:</h4>
     <h4>Github - <a href="https://github.com/Deepakkumar757">Link</a></h4>
     <h4>Email - deepakumar0220@gmail.com</h4></div>
